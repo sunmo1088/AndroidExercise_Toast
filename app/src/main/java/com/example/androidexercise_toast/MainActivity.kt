20e7460035d6f6708684c70891630543386e9990
Sun Mo <sun.mo1088@gmail.com>
2023-05-01 14:40:27 -0600

    added custom toast

--- app/src/main/java/com/example/androidexercise_toast/MainActivity.kt
+++ app/src/main/java/com/example/androidexercise_toast/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.androidexercise_toast
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.example.androidexercise_toast.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val layout = layoutInflater.inflate(R.layout.custom_toast, findViewById<View>(R.id.ll_wrapper) as ViewGroup?)
+
+
+        binding.constraintLayout.setOnClickListener {
+//            Toast.makeText(this, "Hi I'm toast!", Toast.LENGTH_SHORT).show()
+
+//            old way
+//            val toast = Toast(this)
+//            toast.duration
+
+            Toast(this).apply {
+                duration = Toast.LENGTH_LONG
+                view = layout
+            }.show()
+        }
+
+
+
     }
 }
